94c248ab9b9e1bb2094b493c8d54bd5131960d7a
Roman Kassebaum <roman.kassebaum@kdv-dt.de>
2015-03-13 14:11:36 -0100

    Added packages fpr FMX.

---
+++ packages/CBuilder/OnGuardFMXCD.cpp
@@ -0,0 +1,20 @@
+//---------------------------------------------------------------------------
+
+#include <System.hpp>
+#pragma hdrstop
+USEFORMNS("..\..\source\ogabout0Fmx.pas", Ogabout0fmx, OgAboutForm);
+USEFORMNS("..\..\source\onguard6Fmx.pas", Onguard6fmx, ModifierFrm);
+//---------------------------------------------------------------------------
+#pragma package(smart_init)
+//---------------------------------------------------------------------------
+
+//   Package-Quelltext.
+//---------------------------------------------------------------------------
+
+
+#pragma argsused
+extern "C" int _libmain(unsigned long reason)
+{
+	return 1;
+}
+//---------------------------------------------------------------------------
---
+++ packages/CBuilder/OnGuardFMXCR.cpp
@@ -0,0 +1,22 @@
+//---------------------------------------------------------------------------
+
+#include <System.hpp>
+#pragma hdrstop
+USEFORMNS("..\..\source\onguard2Fmx.pas", Onguard2fmx, CodeGenerateFrm);
+USEFORMNS("..\..\source\onguard1Fmx.pas", Onguard1fmx, KeyGenerateFrm);
+USEFORMNS("..\..\source\onguard4Fmx.pas", Onguard4fmx, EditProductFrm);
+USEFORMNS("..\..\source\onguard3Fmx.pas", Onguard3fmx, KeyMaintFrm);
+//---------------------------------------------------------------------------
+#pragma package(smart_init)
+//---------------------------------------------------------------------------
+
+//   Package-Quelltext.
+//---------------------------------------------------------------------------
+
+
+#pragma argsused
+extern "C" int _libmain(unsigned long reason)
+{
+	return 1;
+}
+//---------------------------------------------------------------------------
