7c339d34f6052bb3e0b684397799afc97da327dd
Roman Kassebaum <roman@kassebaum.eu>
2021-05-15 01:59:13 +0200

    Fixed issue  #5.

--- source/VCL.onguard2.pas
+++ source/VCL.onguard2.pas
@@ -231,9 +231,9 @@ begin
     if StringModifierCb.Checked then begin                           {!!.11}
       S := ModStringEd.Text;                                         {!!.11}
       {strip accented characters from the string}                    {!!.12}
-      for i := S.Length downto 0 do                                  {!!.12}
+      for i := S.Length - 1 downto 0 do                              {!!.12}
         if Ord(S.Chars[i]) > 127 then                                {!!.12}
-          S.Remove(i, 1);                                             {!!.12}
+          S.Remove(i, 1);                                            {!!.12}
       L := StringHashELF(S);                                         {!!.11}
     end;                                                             {!!.11}
 
